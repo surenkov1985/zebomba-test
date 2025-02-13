1ff02ecb64579663a2e022899a162a3c6255f138
mich-man <88162731+surenkov1985@users.noreply.github.com>
2025-02-13 21:27:04 +0300

    верстка карты, передвижение персонажа, слайдер друзей(начало)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./assets/game/script";
+import {Game, points} from "./assets/game/script";
+
+const hero = document.querySelector('.hero')
+const game = new Game(hero, points)
+
+game.start()
